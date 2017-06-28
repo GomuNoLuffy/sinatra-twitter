574a3b6321b6227e7ac973f2d2ab008f94478726
teeyingyap <yapteeying@gmail.com>
2017-06-29 03:18:07 +0800

    added suggested users' list

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -2,6 +2,7 @@ get '/' do
   if logged_in? 
   	@following_id = current_user.followed.map(&:id).push(current_user.id)
   	@maintweets = Tweet.where(user_id: @following_id).order("created_at DESC")
+  	@suggested_users = User.where.not(id: @following_id)
    	erb :"static/main"
   else 
 	erb :"static/index" 
